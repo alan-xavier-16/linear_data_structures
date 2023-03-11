8693b1dc4b7ef7762518be40c4f40bc5c5febf49
Alan Xavier <alan_xvr@hotmail.com>
2023-03-11 01:47:29 +0100

    Test Queue

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
 #include "Stack.h"
+#include "Queue.h"
 
 int main()
 {
-  // Create Stack
+  /* ---------------------------------- Stack --------------------------------- */
   Stack *st = new Stack(10);
 
   // Push to stack
@@ -20,4 +21,32 @@ int main()
 
   std::cout << "After pop." << std::endl;
   st->printStack();
+
+  delete st;
+  /* ---------------------------------- Queue --------------------------------- */
+  Queue *q = new Queue(5);
+
+  // Can't remove from empty queue
+  q->deQueue();
+
+  // Add to queue
+  q->enQueue(1);
+  q->enQueue(2);
+  q->enQueue(3);
+  q->enQueue(4);
+  q->enQueue(5);
+
+  // Unable to add to full queue
+  q->enQueue(6);
+
+  std::cout << "Before dequeue." << std::endl;
+  q->printQueue();
+
+  // Remove first element in queue
+  q->deQueue();
+
+  std::cout << "After dequeue." << std::endl;
+  q->printQueue();
+
+  delete q;
 }
